6dcf29d906e9a89e3c08a0c23b547bb914668827
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-04-19 19:36:42 -0400

    Remove extraneous lager statements.

--- src/csm_streaming_test.erl
+++ src/csm_streaming_test.erl
@@ -11,14 +11,10 @@
 %% @doc Mapper which just returns the same key/value pair.
 -spec mapper(term(), term(), term()) -> {ok, {term(), term()}}.
 mapper(_Input, Partition, FD) ->
-    lager:info("Mapper triggered."),
-
     ok = riak_pipe_vnode_worker:send_output({1, 1}, Partition, FD),
     ok.
 
 %% @doc Reducer which just returns the starting accumulator.
 -spec reducer(term(), term(), term(), term()) -> {ok, list()}.
 reducer(_, InAcc, _, _) ->
-    lager:info("Reducer triggered: ~p", [InAcc]),
-
     {ok, InAcc}.
