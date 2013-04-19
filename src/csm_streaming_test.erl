54806c565acd93bee151a15b9804f7920e4678aa
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-04-19 18:19:49 -0400

    Add documentation.

--- src/csm_streaming_test.erl
+++ src/csm_streaming_test.erl
@@ -1,11 +1,18 @@
--module(csm_streaming_test).
+%% @author Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
+%% @copyright 2013 Christopher Meiklejohn.
+%% @doc Helper functions for pipeline testing.
 
--export([mapper/3, reducer/4]).
+-module(csm_streaming_test).
+-author('Christopher Meiklejohn <christopher.meiklejohn@gmail.com>').
+
+-export([mapper/3,
+         reducer/4]).
 
 %% @doc Mapper which just returns the same key/value pair.
 -spec mapper(term(), term(), term()) -> {ok, {term(), term()}}.
 mapper(_Input, Partition, FD) ->
     lager:info("Mapper triggered."),
+
     ok = riak_pipe_vnode_worker:send_output({1, 1}, Partition, FD),
     ok.
 
@@ -13,4 +20,5 @@ mapper(_Input, Partition, FD) ->
 -spec reducer(term(), term(), term(), term()) -> {ok, list()}.
 reducer(_, InAcc, _, _) ->
     lager:info("Reducer triggered: ~p", [InAcc]),
+
     {ok, InAcc}.
