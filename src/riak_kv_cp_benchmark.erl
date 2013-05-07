2e94907dfc312425ae94589c9a46620b4d4c4ba2
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-05-07 15:08:19 -0400

    Modify benchmark helper module.

--- src/riak_kv_cp_benchmark.erl
+++ src/riak_kv_cp_benchmark.erl
@@ -2,7 +2,7 @@
 %% @copyright 2013 Christopher Meiklejohn.
 %% @doc Helper functions for pipeline testing.
 
--module(csm_streaming_test).
+-module(riak_kv_cp_benchmark).
 -author('Christopher Meiklejohn <christopher.meiklejohn@gmail.com>').
 
 -export([mapper/3,
@@ -10,11 +10,16 @@
 
 %% @doc Mapper which just returns the same key/value pair.
 -spec mapper(term(), term(), term()) -> {ok, {term(), term()}}.
+mapper({Key, Value}, Partition, FD) ->
+    Size = byte_size(Value),
+    ok = riak_pipe_vnode_worker:send_output({Key, Size}, Partition, FD),
+    ok;
 mapper(_Input, Partition, FD) ->
     ok = riak_pipe_vnode_worker:send_output({1, 1}, Partition, FD),
     ok.
 
 %% @doc Reducer which just returns the starting accumulator.
 -spec reducer(term(), term(), term(), term()) -> {ok, list()}.
-reducer(_, InAcc, _, _) ->
+reducer(Input, InAcc, Partition, FD) ->
+    ok = riak_pipe_vnode_worker:send_output(Input, Partition, FD),
     {ok, InAcc}.
