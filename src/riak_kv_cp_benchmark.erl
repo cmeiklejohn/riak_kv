8edf523369b390cf0c2e037a6ef444d58f667995
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-05-17 17:13:32 -0700

    Add benchmarking instrumentation.

--- src/riak_kv_cp_benchmark.erl
+++ src/riak_kv_cp_benchmark.erl
@@ -11,15 +11,22 @@
 %% @doc Mapper which just returns the same key/value pair.
 -spec mapper(term(), term(), term()) -> {ok, {term(), term()}}.
 mapper({Key, Value}, Partition, FD) ->
+    lager:warning("Mapper received: ~p", [{Key, Value}]),
     Size = byte_size(Value),
-    ok = riak_pipe_vnode_worker:send_output({Key, Size}, Partition, FD),
+    Result = {{Key, Size}, Value},
+    ok = riak_pipe_vnode_worker:send_output(Result, Partition, FD),
+    lager:warning("Mapper sent: ~p", [Result]),
     ok;
-mapper(_Input, Partition, FD) ->
+mapper(Input, Partition, FD) ->
+    lager:warning("Mapper received: ~p", [Input]),
     ok = riak_pipe_vnode_worker:send_output({1, 1}, Partition, FD),
+    lager:warning("Mapper sent: ~p", [{1, 1}]),
     ok.
 
 %% @doc Reducer which just returns the starting accumulator.
 -spec reducer(term(), term(), term(), term()) -> {ok, list()}.
 reducer(Input, InAcc, Partition, FD) ->
+    lager:warning("Reducer received: ~p", [Input]),
     ok = riak_pipe_vnode_worker:send_output(Input, Partition, FD),
+    lager:warning("Reducer sent: ~p", [Input]),
     {ok, InAcc}.
